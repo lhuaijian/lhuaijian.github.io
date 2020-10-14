069ee786b825064cde26ed006d2c3a8159553d07
lhuaijian <357828205@qq.com>
2020-10-14 21:54:21 +0800

    all

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,4 +43,22 @@ $(function () {
     	})
     }
   })
-})
+}
+
+  $("#back-top").hide();
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn()
+      } else {
+        $('#back-top').fadeOut()
+      }
+    });
+    $('#back-top a').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800)
+      return false
+    })
+  })
+)
